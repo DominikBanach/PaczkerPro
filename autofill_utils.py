01475f9c28544040b5b48734c309232d9be04810
Dominik Banach <65920764+DominikBanach@users.noreply.github.com>
2025-11-19 14:43:08 +0100

    feat: autofill_utlis.py and example.csv added

---
+++ autofill_utils.py
@@ -0,0 +1,22 @@
+import csv
+
+def _load_data(path):
+    """
+    Loads csv file form path and returns it as python dict for fast access.
+    """
+    data = {}
+    with open(path, mode='r', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if not row: continue
+            try: data[row[0]] = tuple(map(float, row[1:]))
+            except ValueError: print(f"Pominięto wiersz z błędem danych: {row}"); continue
+    return data
+
+def get_item_as_tuple_by_reference(data, ref):
+    """
+    Tries to get item info from given dict and returns item as tuple. Ready to pass to packing_utils._normalized_item().
+    """
+    item_dims = data.get(ref, None)
+    if not item_dims: return None
+    return (ref, *item_dims)
