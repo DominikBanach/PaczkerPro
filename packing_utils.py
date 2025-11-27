257de1ab2c7f5af387c1bfb4424d55aef769f57b
Dominik Banach <dominikbanach5@gmail.com>
2025-11-27 17:21:08 +0100

    feat: margins logic and description

--- packing_utils.py
+++ packing_utils.py
@@ -62,6 +62,32 @@ def _try_packing_into_all_possible_bins(items):
     return [_try_packing(bin, _tuples_to_items(items)) for bin in _get_bins_list()]
 
 
+def _check_margin(bin):
+    """
+    Returns margin understood as smallest non-zero length between item and bin face.
+    Returns zero in case of "perfect fit".
+    Returns biggest bin dimension in case of empty bin.
+    """
+    margin = float("inf")
+    tolerance = 1e-2
+    bin_limits = [bin.width, bin.height, bin.depth]
+
+    if not bin.items: return max(bin_limits)
+
+    for item in bin.items:
+        for bin_limit, position, dimension in zip(bin_limits, item.position, item.get_dimension()):
+            local_margin = bin_limit - (position + dimension)
+            if 0 <= local_margin < tolerance: continue
+            margin = min(margin, local_margin)
+
+    if margin < 0:
+        raise ValueError("Calculated margin below zero.")
+    elif margin == float("inf"):
+        return 0.0
+    else:
+        return margin
+
+
 def describe_packability(items):
     """
     Returns string describing given items list packability for all avaliable parcel sizes.
@@ -82,6 +108,7 @@ def describe_packability(items):
         unfitted_items_count = len(unfitted_items)
         bin_name = bin.name
         success_icon = '❌' if unfitted_items_count else '✅'
+        margin = _check_margin(bin)
 
         description += f"{success_icon} | {bin_name}"
 
@@ -91,6 +118,6 @@ def describe_packability(items):
                 description += f"\t- {item.name}\n"
 
         else:  
-            description += "\n"
+            description += f"\t margin: {margin:.2f}cm{" ⚠️⚠️⚠️" if margin <= 1 else ""}\n"
     
     return description
